c9aa820eececb7d40af00c141a7a1320c9db8909
silverforge <janos.murvaigaal@gmail.com>
2013-05-26 10:59:28 +0200

    mgj : comments + generic Get

--- SilverForge.DIvsSL/FruitLocator.cs
+++ SilverForge.DIvsSL/FruitLocator.cs
@@ -3,24 +3,51 @@ using SilverForge.DIvsSL.Fruits;
 
 namespace SilverForge.DIvsSL
 {
+	/// <summary>
+	/// Sample implememtation of Service locator
+	/// </summary>
 	public class FruitLocator
 	{
+		/// <summary>
+		/// The container
+		/// </summary>
 		private readonly IUnityContainer _container = new UnityContainer();
 
+		/// <summary>
+		/// Constructor of <see cref="FruitLocator"/>
+		/// </summary>
 		public FruitLocator()
 		{
 			_container.RegisterType<IApple, Apple>();
 			_container.RegisterType<IBanana, Banana>();
 		}
 
+		/// <summary>
+		/// Gets implementation of IApple
+		/// </summary>
+		/// <returns>Apple instance</returns>
 		public IApple GetApple()
 		{
 			return _container.Resolve<IApple>();
 		}
 
+		/// <summary>
+		/// Gets implementation of IBanana
+		/// </summary>
+		/// <returns>Banana instance</returns>
 		public IBanana GetBanana()
 		{
 			return _container.Resolve<IBanana>();
+		}
+
+		/// <summary>
+		/// Gets instance of T from container
+		/// </summary>
+		/// <typeparam name="T">The type of registered instance</typeparam>
+		/// <returns>The instance</returns>
+		public T Get<T>()
+		{
+			return _container.Resolve<T>();
 		}
 	}
 }
--- SilverForge.DIvsSL/Mixer.cs
+++ SilverForge.DIvsSL/Mixer.cs
@@ -2,17 +2,35 @@
 
 namespace SilverForge.DIvsSL
 {
+	/// <summary>
+	/// Mixer class
+	/// </summary>
 	public class Mixer
 	{
+		/// <summary>
+		/// private IApple member
+		/// </summary>
 		private readonly IApple _apple;
+		/// <summary>
+		/// Private IBanana member
+		/// </summary>
 		private readonly IBanana _banana;
 
+		/// <summary>
+		/// Constructor of <see cref="Mixer"/>
+		/// </summary>
+		/// <param name="apple">Implementation of IApple instance</param>
+		/// <param name="banana">Implementation of IBanana instance</param>
 		public Mixer(IApple apple, IBanana banana)
 		{
 			_apple = apple;
 			_banana = banana;
 		}
 
+		/// <summary>
+		/// Executes the mixing
+		/// </summary>
+		/// <returns>Taste of mixture</returns>
 		public Tastes DoIt()
 		{
 			if (_apple == null)
